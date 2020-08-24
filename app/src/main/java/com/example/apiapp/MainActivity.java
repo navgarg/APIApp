13b52d37ec488dfa549d0121daab84291f9a42df
Navya Garg <navgarg06@gmail.com>
2020-08-24 18:52:14 +0530

    Added GET using volley

--- app/src/main/java/com/example/apiapp/MainActivity.java
+++ app/src/main/java/com/example/apiapp/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.apiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "in onCreate");
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://httpbin.org/get?param1=hello";
+
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        Log.d("Response: ", response.toString());
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Response: ", String.valueOf(error));
+                    }
+                }
+        );
+
+        queue.add(getRequest);
     }
 }
