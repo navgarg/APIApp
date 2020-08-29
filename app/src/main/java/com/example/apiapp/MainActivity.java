00e4f3516fa87fad6cfe871a39783dcd018526f5
Navya Garg <navgarg06@gmail.com>
2020-08-29 11:51:26 +0530

    Added volley requests

--- app/src/main/java/com/example/apiapp/MainActivity.java
+++ app/src/main/java/com/example/apiapp/MainActivity.java
@@ -3,16 +3,25 @@ package com.example.apiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.format.Time;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,16 +31,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "in onCreate");
 
+        Date currentTime = Calendar.getInstance().getTime();
+        Log.d("Date", String.valueOf(currentTime));
+
+        Time today = new Time(Time.getCurrentTimezone());
+        today.setToNow();
+
+        Log.d("Date", String.valueOf(today));
+
         RequestQueue queue = Volley.newRequestQueue(this);
-        final String url = "http://httpbin.org/get?param1=hello";
+        final String url = "https://ysv7zypxt5.execute-api.us-west-2.amazonaws.com/dev/rides?user_id=E0VJnE0YitPFQhy25F3efPD1PXt2&datetime=2020-07-15";
 
         JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>()
                 {
                     @Override
                     public void onResponse(JSONObject response) {
-                        // display response
-                        Log.d("Response: ", response.toString());
+                        Log.d("Response: ", String.valueOf(response));
                     }
                 },
                 new Response.ErrorListener()
@@ -44,5 +60,87 @@ public class MainActivity extends AppCompatActivity {
         );
 
         queue.add(getRequest);
+
+        StringRequest postRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", String.valueOf(error));
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Trip_ID", "10");
+                params.put("User_ID", "E0VJnE0YitPFQhy25F3efPD1PXt2");
+                params.put("Trip_Distance", "7");
+                params.put("Trip_Earning", "50000");
+                params.put("Trip_Month", "August");
+                params.put("Trip_Duration", "4h 50m");
+
+
+                return super.getParams();
+            }
+        };
+        queue.add(postRequest);
+
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", String.valueOf(error));
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String> ();
+                params.put("name", "Alif");
+                params.put("domain", "http://itsalif.info");
+
+                return params;
+            }
+
+        };
+
+        queue.add(putRequest);
+
+        StringRequest dr = new StringRequest(Request.Method.DELETE, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", String.valueOf(error));
+                    }
+                }
+        );
+        queue.add(dr);
+
     }
 }
